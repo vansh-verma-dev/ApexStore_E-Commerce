35c259076336292c3d7e1a2a5f18bfe69ee3b8d6
Vansh Verma <vanshverma6065@gmail.com>
2026-08-14 10:48:27 +0530

    Create a Banner

---
+++ frontend/src/components/hero.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Hero() {
+    const banner = [
+        "https://i.pinimg.com/1200x/25/cb/3b/25cb3bfc1d033a5cb20fe4f2f7d299e0.jpg",
+        "https://i.pinimg.com/1200x/fc/14/21/fc14211c14b1c62c65bd5995cfa3d6d7.jpg",
+        "https://i.pinimg.com/1200x/a4/e8/7f/a4e87f77a37610077e81af5287f95163.jpg"
+    ]
+
+    const [current_Banner, setCurrent_Banner] = useState(0);
+
+    useEffect(() => {
+        const intervel = setInterval(() => {
+            setCurrent_Banner((prev) =>
+                prev === banner.length - 1 ? 0 : prev + 1
+            );
+        }, 3000)
+        return () => clearInterval(intervel);
+    }, []);
+
+    return (
+        <div className="bg-red-600 h-[70vh]">
+            <img src={banner[current_Banner]} alt="APEXSTORE"
+                className="w-full h-full object-cover"
+            />
+        </div>
+    )
+}
+export default Hero;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,11 @@
+import Hero from "../components/hero";
 import Navbar from "../components/navbar";
 
 function HomePage(){
     return(
         <>
         <Navbar/>
+        <Hero/>
         </>
     )
 }
